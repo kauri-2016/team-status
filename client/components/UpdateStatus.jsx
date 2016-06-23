22d9f2016eeda0b43664205ca1e4c2d4c8124519
EDA <eda@mac-mini-6.local>
2016-06-24 11:57:42 +1200

    write minimal UpdateStatus component

---
+++ client/components/UpdateStatus.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default () => {
+  return (
+    <div className='update-status' >
+      <h1>New Status</h1>
+      <input type='text'/>
+      <button>Cancel</button>
+      <button>Update status</button>
+    </div>
+  )
+}
