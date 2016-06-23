2611f1f32a95907d6d819fa4625918e31160e179
Abhi <abhi.kala123@gmail.com>
2016-06-24 11:13:57 +1200

    styled landing page with buttons using skeleton

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -6,7 +6,6 @@ export default React.createClass({
   render () {
     return (
       <div>
-        <h1>Home</h1>
         <Landing />
       </div>
     )
--- client/components/Landing.jsx
+++ client/components/Landing.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 
 export default (props) => (
   <div>
-    <h1>Landing Page</h1>
-    <div className="view-board">
-      <a href="#">View Board</a>
-    </div>
-    <div className="get-status">
-      <a href="#">Update Status</a>
+    <div className="container">
+      <h1 className="heading">Team Status</h1>
+      <div className="view-board">
+        <a className="button" href="#">View Board</a>
+      </div>
+      <div className="get-status">
+        <a  className="button" href="#">Update Status</a>
+      </div>
     </div>
   </div>
 )
