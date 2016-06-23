eaef4d9886f3141ccd85a1e48b501e61973fb62b
Abhi <abhi.kala123@gmail.com>
2016-06-24 11:30:06 +1200

    implemented react-router/landing page live

--- client/index.js
+++ client/index.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import { render } from 'react-dom'
+import { Router, Route, Link, hashHistory, IndexRoute } from 'react-router'
 
 import App from './components/App'
+import Landing from './components/Landing'
 
 
 document.addEventListener('DOMContentLoaded', () => {
   render(
-    <App />,
+    <Router history={hashHistory}>
+      <Route path="/" component={App}>
+        <IndexRoute component={Landing}/>
+      </Route>
+    </Router>,
     document.getElementById('app')
   )
 })
