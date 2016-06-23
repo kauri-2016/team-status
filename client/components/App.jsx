8574492770703cf84c0b91d32337c85469ef58d4
Abhi <abhi.kala123@gmail.com>
2016-06-24 10:58:35 +1200

    basic landing page created

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 
+import Landing from './Landing'
+
 export default React.createClass({
   render () {
     return (
       <div>
         <h1>Home</h1>
+        <Landing />
       </div>
     )
   }
---
+++ client/components/Landing.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default (props) => (
+  <div>
+    <h1>Landing Page</h1>
+    <div className="view-board">
+      <a href="#">View Board</a>
+    </div>
+    <div className="get-status">
+      <a href="#">Update Status</a>
+    </div>
+  </div>
+)
