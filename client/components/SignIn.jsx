fd7856a44da766dc19209612912ee74f72de613b
Devon McGrath <devon.mcgrath@gmail.com>
2016-06-24 10:53:44 +1200

    Add files

---
+++ client/components/SignIn.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default React.createClass({
+  render () {
+    return (
+        <div>
+            <h1>Sign In</h1>
+            <form
+        </div>
+
+    )
+  }
+})
