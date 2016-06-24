49e44b99b72068a63f5cb7d25cd56852653af8f1
josh-jacobsen <joshjacobsen5@gmail.com>
2016-06-24 14:52:16 +1200

    fixed bad merge

--- client/index.js
+++ client/index.js
@@ -5,11 +5,7 @@ import { Provider } from 'react-redux'
 
 import reducer from './reducer'
 import App from './components/App'
-import reducer from './reducer'
 
-let store = createStore(reducer)
-
-console.log("state on index.js - ",store.getState());
 
 let store = createStore(
   reducer, compose(
