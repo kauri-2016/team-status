10d1a07a04a5380732fc3b100e9522f6b5b07ff2
ben-southpaw <ben.dryden@gmail.com>
2016-06-24 12:19:45 +1200

    basic redux setup

--- client/index.js
+++ client/index.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import { render } from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 
+import reducer from './reducer'
 import App from './components/App'
+
+const store = createStore(reducer)
 
 document.addEventListener('DOMContentLoaded', () => {
   render(
-    <App />,
+    <Provider store={store}>
+      <App />
+    </Provider>,
     document.getElementById('app')
   )
 })
